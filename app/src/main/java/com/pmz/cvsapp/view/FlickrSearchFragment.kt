5768abf4b2ac79d9e94cd309e3b253fff57b6824
PhatherJean <patrice-jean@lambdastudents.com>
2021-12-09 15:36:43 -0500

    Working on creating a mockito test for the unit test

--- app/src/main/java/com/pmz/cvsapp/view/FlickrSearchFragment.kt
+++ app/src/main/java/com/pmz/cvsapp/view/FlickrSearchFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -50,6 +51,10 @@ class FlickrSearchFragment : Fragment() {
                 }
                 setOnClickListener {
                     viewModel.getSearchQueries()
+                }
+                addTextChangedListener {
+                    if(it.toString().isNotBlank())
+                        viewModel.query = it.toString()
                 }
             }
             searchBtn.setOnClickListener {
